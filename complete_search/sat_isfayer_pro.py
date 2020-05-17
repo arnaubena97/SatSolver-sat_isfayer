3175e30ca4793e2025d9051e509bfbe5baad1a5b
arnaubena97 <arnaubenavides97@gmail.com>
2020-05-17 13:30:21 +0200

    version v2

--- complete_search/sat_isfayer_pro.py
+++ complete_search/sat_isfayer_pro.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import os
 import sys
-import random
 from collections import defaultdict
 sys.setrecursionlimit(10**9)
-
 
 
 def read_file(file_name):
@@ -22,18 +19,22 @@ def read_file(file_name):
             clauses.append(clause)
     return num_variables, clauses
 
-def print_solution(solution):
+def print_solution(solution, num_vars):
     """Method to print the solution that satisfies all the clauses or unsatisfiable """
     if solution:
-        #solution = sorted(solution, key=abs)
+        aux_solution = [x for x in range(1, num_vars+1)]
+        for var in solution:
+            if aux_solution[abs(var)-1] != var:
+                aux_solution[abs(var)-1] = var
         print("s SATISFIABLE")
-        print("v %s 0"  %" ".join(map(str, solution)))
+        print("v %s 0"  %" ".join(map(str, aux_solution)))
         exit(0)
     else:
         print("s UNSATISFIABLE")
         exit(0)
 
 def select_var1(clauses):
+    """heuristic1"""
     literal_weight = defaultdict(int)
     for clause in clauses:
         for literal in clause:
@@ -41,6 +42,7 @@ def select_var1(clauses):
     return max(literal_weight, key=literal_weight.get)
 
 def select_var2(clauses):
+    """heuristic2"""
     counter = defaultdict(int)
     for clause in clauses:
         for literal in clause:
@@ -50,8 +52,8 @@ def select_var2(clauses):
                 counter[literal] = 2 ** -len(clause)
     return max(counter, key=counter.get)
 
-
 def select_var3(clauses):
+    """heuristic3"""
     counter = defaultdict(int)
     for clause in clauses:
         for literal in clause:
@@ -61,62 +63,54 @@ def select_var3(clauses):
                 counter[literal] = 1
     return max(counter, key=counter.get)
 
-def backtrack(clauses , interpretation):
+def backtrack(clauses, interpretation):
     clauses, var = unit_prop(clauses)
     interpretation = interpretation + var
-    if clauses == "C":
+    if clauses == "conflict":
         return []
     if not clauses:
         return interpretation
-    best_var =select_var2(clauses)
-    return (backtrack(delete_aparitions(clauses, best_var), interpretation +[best_var]) or backtrack(delete_aparitions(clauses, -best_var), interpretation + [-best_var]))
+    best_var =select_var1(clauses)
+    return(backtrack(delete_aparitions(clauses, best_var), interpretation +[best_var])
+           or backtrack(delete_aparitions(clauses, -best_var), interpretation + [-best_var]))
 
-
-def delete_aparitions(clauses, var):
-    aux_clauses= []
+def delete_aparitions(clauses, variable):
+    """delete apartitions of variable or -variable in clause"""
+    aux_clauses = []
     for clause in clauses:
-        if var not in clause:
-            if -var not in clause:
-                aux_clauses.append(clause)
-            else:
-                aux_clause = []
-                for aux_var in clause:
-                    if aux_var is not -var:
-                        aux_clause.append(aux_var)
-                if not aux_clause:
-                    return "C"
-                aux_clauses.append(aux_clause)
+        if -variable in clause:
+            one_clause =[]
+            for var in clause:
+                if var != -variable:
+                    one_clause.append(var)
+            if one_clause == []:
+                return "conflict"
+            aux_clauses.append(one_clause)
+        if variable not in clause and -variable not in clause:
+            aux_clauses.append(clause)
     return aux_clauses
 
-def sel_lenght1(clauses):
+def find_lenght1(clauses):
+    """find the clauses of lenght 1 to satisfy"""
     aux_clauses = []
     for clause in clauses:
         if len(clause)==1:
             aux_clauses.append(clause[0])
     return aux_clauses
 
-# This implements the while loop of the BCP function
 def unit_prop(clauses):
     interpretation= []
-    clauses_len1 = sel_lenght1(clauses) #llista de clausules de llargada 1
+    clauses_len1 = find_lenght1(clauses) #llista de clausules de llargada 1
     while clauses_len1:
         var = clauses_len1.pop() #per totes les clausules que estan soles...
         clauses = delete_aparitions(clauses, var)
         interpretation+= [var]
-        if clauses == "C":
-            return "C", []
+        if clauses == "conflict":
+            return "conflict", []
         if not clauses: #si ja no tenim mes clausules, retornem les clusules [] i la interpretacio
             return [], interpretation
-        clauses_len1 = sel_lenght1(clauses)#mirem si hi ha alguna variable que hagi pugut quedar sola
+        clauses_len1 = find_lenght1(clauses)#mirem si hi ha alguna variable que hagi pugut quedar sola
     return clauses, interpretation
-
-def write_file(solution):
-    f = open("t.txt", "w")
-    if solution:
-        f.write("s SATISFIABLE\n")
-        f.write(("v %s 0" % " ".join(map(str, solution))))
-    else:
-        f.write("s UNSATISFIABLE")
 
 #Main
 if __name__ == "__main__":
@@ -126,13 +120,9 @@ if __name__ == "__main__":
         print("\n Command: python %s <file_name.cnf> \n" %sys.argv[0])
         exit(0)
 
-    count = 0
     vars, clauses = read_file(file_name)
     solution = backtrack(clauses,[])
-    #print_solution(solution)
+    print_solution(solution, vars)
 
-    write_file(solution)
-    command = "python3 comprove.py " + file_name + " " + "t.txt"
-    os.system(command)
 
 
